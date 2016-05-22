8b7e07ce923d301e4015335d187cd0a2fc710dc0
Laurent Margirier <laurent.margirier@gmail.com>
2016-05-22 19:24:01 +0100

    feat(*): add prototype service worker

--- src/index.js
+++ src/index.js
@@ -51,6 +51,19 @@ export const store = createStore(
   //)
 )
 
+console.log("why")
+if ('serviceWorker' in navigator) {
+  console.log("guuu")
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
+
+
 window.__store = store
 
 const history = syncHistoryWithStore(browserHistory, store)
